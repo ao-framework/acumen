07bb5bc46fe5b2c58340c282259681af9b4d734f
Josh Miller <wickedhillsmedia@gmail.com>
2019-12-30 16:41:53 -0500

    added header contract dist files

---
+++ dist/src/contracts/headers/iBaseHeaders.d.ts
@@ -0,0 +1,6 @@
+import { iKeyValuePair } from "../base/iKeyValuePair";
+import { iEntryData } from "./iEntryData";
+export interface iBaseHeaders {
+    entry: iEntryData;
+    data: iKeyValuePair<string>;
+}
